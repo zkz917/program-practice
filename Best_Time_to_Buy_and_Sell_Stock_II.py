22593944e6fbf235cf236adccda930610ffa4a71
Kuangzheng Zhou <zkz917@126.com>
2017-04-19 18:58:25 -0400

    Update Best_Time_to_Buy_and_Sell_Stock_II.py

--- Best_Time_to_Buy_and_Sell_Stock_II.py
+++ Best_Time_to_Buy_and_Sell_Stock_II.py
@@ -1,1 +1,13 @@
-
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if len(prices)<2:
+            return 0
+        maxval = 0
+        for i in range(1,len(prices)):
+            if prices[i] > prices[i-1]:
+                maxval += prices[i] - prices[i-1]
+        return maxval
