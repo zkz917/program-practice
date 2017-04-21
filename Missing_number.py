32748ce42d32f5a6834f6230d5155f90b38d7078
Kuangzheng Zhou <zkz917@126.com>
2017-04-21 12:28:06 -0400

    Create Missing_number.py

---
+++ Missing_number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = len(nums)
+        
+        for i in range(len(nums)):
+            res = res^i^nums[i]
+        
+        return res
