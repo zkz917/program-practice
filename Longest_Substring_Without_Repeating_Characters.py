57e2a3e22de39b258bf5322c0cb81acdfec8f122
Kuangzheng Zhou <zkz917@126.com>
2017-04-22 15:58:15 -0400

    Update Longest_Substring_Without_Repeating_Characters.py

--- Longest_Substring_Without_Repeating_Characters.py
+++ Longest_Substring_Without_Repeating_Characters.py
@@ -28,3 +28,19 @@ class Solution(object):
     
     def duplicated(self,s):
         return len(s) != len(set(s))
+#############################################################################
+class Solution:
+    # @return an integer
+    def lengthOfLongestSubstring(self, s):
+        start = maxLength = 0
+        usedChar = {}
+        
+        for i in range(len(s)):
+            if s[i] in usedChar and start <= usedChar[s[i]]:
+                start = usedChar[s[i]] + 1
+            else:
+                maxLength = max(maxLength, i - start + 1)
+
+            usedChar[s[i]] = i
+
+        return maxLength
